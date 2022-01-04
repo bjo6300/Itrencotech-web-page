a26c3461ed12f02c2d6dee96211ba4b41fdc0b11
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-01-04 12:23:33 +0900

    asd

--- itrencotech/views.py
+++ itrencotech/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("안녕하세요 itrencotech에 오신것을 환영합니다.")
+    return render(request, 'templates/body/onlyIdea/onlyIdea.html')
+
+
