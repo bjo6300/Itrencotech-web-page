4dd91ca439de701ef87f727eaf7444eea3a551e6
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-01-04 15:03:00 +0900

    onlyIdea 하위 페이지

--- itrencotech/urls.py
+++ itrencotech/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('body/onlyIdea/onlyIdea_home/', views.body_onlyIdea_onlyIdea, name='onlyIdea'),
     path('body/prototyping/prototyping_home/', views.body_prototyping_prototyping_home, name='prototyping'),
-    
+
+    path('body/onlyIdea/onlyIdea_mechanical_design/', views.body_onlyIdea_onlyIdea_mechanical_design, name='mechanical_design'), # 기계설계
+    path('body/onlyIdea/onlyIdea_product_mechanism_design/', views.body_onlyIdea_onlyIdea_product_mechanism_design, name='product_mechanism_design'), # 제품기구설계
+    path('body/onlyIdea/onlyIdea_production_design/', views.body_onlyIdea_onlyIdea_production_design, name='production_design'), # 제품디자인
     
 ]
--- itrencotech/views.py
+++ itrencotech/views.py
@@ -9,4 +9,17 @@ def body_onlyIdea_onlyIdea(request):
     return render(request, 'body/onlyIdea/onlyIdea_home.html')
 
 def body_prototyping_prototyping_home(request):
+    # 시제품 제작 메인
     return render(request, 'body/prototyping/prototyping_home.html')
+
+def body_onlyIdea_onlyIdea_mechanical_design(request): 
+    # 기계설계
+    return render(request, 'body/onlyIdea/onlyIdea_mechanical_design.html')
+
+def body_onlyIdea_onlyIdea_product_mechanism_design(request):
+    # 제품 기구설계
+    return render(request, 'body/onlyIdea/onlyIdea_product_mechanism_design.html')
+
+def body_onlyIdea_onlyIdea_production_design(request):
+    # 제품 디자인
+    return render(request, 'body/onlyIdea/onlyIdea_production_design.html')
