34c9dda8d7af81968a0371442d42ae239f742307
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-01-04 14:31:06 +0900

    onlyIdea, prototyping 초안 / home 임시로(변경예정)

--- itrencotech/urls.py
+++ itrencotech/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('body/onlyIdea/onlyIdea_home/', views.body_onlyIdea_onlyIdea, name='onlyIdea'),
+    path('body/prototyping/prototyping_home/', views.body_prototyping_prototyping_home, name='prototyping'),
+    
+    
 ]
--- itrencotech/views.py
+++ itrencotech/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 
 def index(request):
-    return HttpResponse("안녕하세요 itrencotech에 오신것을 환영합니다.")
+    return render(request, 'home.html')
+
+def body_onlyIdea_onlyIdea(request):
+    return render(request, 'body/onlyIdea/onlyIdea_home.html')
+
+def body_prototyping_prototyping_home(request):
+    return render(request, 'body/prototyping/prototyping_home.html')
