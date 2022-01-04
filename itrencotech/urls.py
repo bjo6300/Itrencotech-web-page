54cc23eacb52af1e9a20b7a3568a81ea72bd59a1
현경 <nhk1657@kpu.ac.kr>
2022-01-04 14:29:39 +0900

    포트폴리오 탭 세분화

--- itrencotech/urls.py
+++ itrencotech/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'itrencotech'
+
 urlpatterns = [
-    path('', views.index),
-    path('portfolio/', views.portfolio),
-    path('review/', views.review)
+    path('', views.index, name='index'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('portfolio/3d', views.portfolio3d, name='portfolio_3d'),
+    path('portfolio/cnc', views.portfolioCnc, name='portfolio_cnc'),
+    path('portfolio/mockup', views.portfolioMockup, name='portfolio_mockup'),
+    path('portfolio/mold', views.portfolioMold, name='portfolio_mold'),
+    path('review/', views.review, name='portfolio_review'),
 ]
--- itrencotech/views.py
+++ itrencotech/views.py
@@ -12,6 +12,26 @@ def portfolio(request):
     return render(request, 'navBar/portfolio/portfolio.html')
 
 
+def portfolio3d(request):
+    """ 포트폴리오 - 3D 프린팅 페이지 """
+    return render(request, 'navBar/portfolio/3d.html')
+
+
+def portfolioCnc(request):
+    """ 포트폴리오 - CNC 페이지 """
+    return render(request, 'navBar/portfolio/cnc.html')
+
+
+def portfolioMockup(request):
+    """ 포트폴리오 - 목업 페이지 """
+    return render(request, 'navBar/portfolio/mockup.html')
+
+
+def portfolioMold(request):
+    """ 포트폴리오 - 사출 금형 페이지 """
+    return render(request, 'navBar/portfolio/mold.html')
+
+
 def review(request):
     """ 후기 페이지 """
     return render(request, 'navBar/review/review.html')
