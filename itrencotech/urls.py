c85aaf4890765163a39156a3c84a5d8642fbac6c
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-01-04 15:38:40 +0900

    fix url

--- itrencotech/urls.py
+++ itrencotech/urls.py
@@ -4,16 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('body/onlyIdea/onlyIdea_home/', views.body_onlyIdea_onlyIdea, name='onlyIdea'),
-    path('body/prototyping/prototyping_home/', views.body_prototyping_prototyping_home, name='prototyping'),
+    path('onlyIdea/onlyIdea_home/', views.body_onlyIdea_onlyIdea, name='onlyIdea'),
+    path('prototyping/prototyping_home/', views.body_prototyping_prototyping_home, name='prototyping'),
 
-    path('body/onlyIdea/onlyIdea_mechanical_design/', views.body_onlyIdea_onlyIdea_mechanical_design, name='mechanical_design'), # 기계설계
-    path('body/onlyIdea/onlyIdea_product_mechanism_design/', views.body_onlyIdea_onlyIdea_product_mechanism_design, name='product_mechanism_design'), # 제품기구설계
-    path('body/onlyIdea/onlyIdea_production_design/', views.body_onlyIdea_onlyIdea_production_design, name='production_design'), # 제품디자인
+    path('onlyIdea/onlyIdea_mechanical_design/', views.body_onlyIdea_onlyIdea_mechanical_design, name='mechanical_design'), # 기계설계
+    path('onlyIdea/onlyIdea_product_mechanism_design/', views.body_onlyIdea_onlyIdea_product_mechanism_design, name='product_mechanism_design'), # 제품기구설계
+    path('onlyIdea/onlyIdea_production_design/', views.body_onlyIdea_onlyIdea_production_design, name='production_design'), # 제품디자인
     
-    path('body/prototyping/3DPrinting/', views.body_prototyping_3Dprinting, name='3Dprinting'), # 3D 프린팅
-    path('body/prototyping/mockUp/', views.body_prototyping_mockUp, name='mockUp'), # 목업
-    path('body/prototyping/CNC/', views.body_prototyping_CNC, name='CNC'), # CNC 가공
-    path('body/prototyping/sheeting_bending/', views.body_prototyping_sheeting_bending, name='sheeting_bending'), # 판금 절곡
+    path('prototyping/3DPrinting/', views.body_prototyping_3Dprinting, name='3Dprinting'), # 3D 프린팅
+    path('prototyping/mockUp/', views.body_prototyping_mockUp, name='mockUp'), # 목업
+    path('prototyping/CNC/', views.body_prototyping_CNC, name='CNC'), # CNC 가공
+    path('prototyping/sheeting_bending/', views.body_prototyping_sheeting_bending, name='sheeting_bending'), # 판금 절곡
     
 ]
