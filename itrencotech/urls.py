6ca9d1bbb4dd1e6c15a6a8969f2375b61c259f8b
Yeseong31 <yeseong31@naver.com>
2022-01-04 13:24:56 +0900

    0104 About, 양산 추가

--- itrencotech/urls.py
+++ itrencotech/urls.py
@@ -6,4 +6,9 @@ app_name = 'itrencotech'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('navBar/about/ceo/', views.navbar_about_ceo, name='navbar_about_ceo'),
+    path('navBar/about/history/', views.navbar_about_history, name='navbar_about_history'),
+    path('navBar/about/ideology/', views.navbar_about_ideology, name='navbar_about_ideology'),
+    path('navBar/about/route/', views.navbar_about_route, name='navbar_about_route'),
+    path('body/production/home/', views.body_production_home, name='body_production_home'),
 ]
