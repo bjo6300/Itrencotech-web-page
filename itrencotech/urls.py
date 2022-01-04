63de1170daf02b08ff191004207cfce5b53b0bdd
eneleh758 <78579586+eneleh758@users.noreply.github.com>
2022-01-04 15:52:12 +0900

    로그인, 하위 페이지 구현

--- itrencotech/urls.py
+++ itrencotech/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'itrencotech'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('signUp/', views.signUp, name='signUp'),
+    path('findIdPasswd/', views.findIdPasswd, name='findIdPasswd'),
+    path('findId/', views.findId, name='findId'),
+
 ]
--- itrencotech/views.py
+++ itrencotech/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("안녕하세요 itrencotech에 오신것을 환영합니다.")
+
+def login(request):
+    return render(request, 'login/login.html')
+
+def signUp(request):
+    return render(request, 'login/signUp.html')
+
+def findIdPasswd(request):
+    return render(request, 'login/findIdPasswd.html')
+
+def findId(request):
+    return render(request, 'login/findId.html')
