d2c63c4a86a30458badf41718b3178e4d0747bd9
Yeseong31 <yeseong31@naver.com>
2022-01-04 14:27:17 +0900

    0104 Fixed Error

--- itrencotech/urls.py
+++ itrencotech/urls.py
@@ -6,12 +6,12 @@ app_name = 'itrencotech'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('navBar/about/ceo/', views.navbar_about_ceo, name='navbar_about_ceo'),
-    path('navBar/about/history/', views.navbar_about_history, name='navbar_about_history'),
-    path('navBar/about/ideology/', views.navbar_about_ideology, name='navbar_about_ideology'),
-    path('navBar/about/route/', views.navbar_about_route, name='navbar_about_route'),
-    path('body/production/home/', views.body_production_home, name='body_production_home'),
-    path('body/production/mold/', views.body_production_mold, name='body_production_mold'),
-    path('body/production/cnc/', views.body_production_cnc, name='body_production_cnc'),
-    path('body/production/press/', views.body_production_press, name='body_production_press'),
+    path('about/ceo/', views.navbar_about_ceo, name='navbar_about_ceo'),
+    path('about/history/', views.navbar_about_history, name='navbar_about_history'),
+    path('about/ideology/', views.navbar_about_ideology, name='navbar_about_ideology'),
+    path('about/route/', views.navbar_about_route, name='navbar_about_route'),
+    path('production/home/', views.body_production_home, name='body_production_home'),
+    path('production/mold/', views.body_production_mold, name='body_production_mold'),
+    path('production/cnc/', views.body_production_cnc, name='body_production_cnc'),
+    path('production/press/', views.body_production_press, name='body_production_press'),
 ]
