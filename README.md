c938b9b64d24b444464217dd9c5f0d800397b4ec
Yeseong31 <yeseong31@naver.com>
2022-01-04 17:23:30 +0900

    0104 링크 수정(2)

---
+++ README.md
@@ -0,0 +1,34 @@
+![Python: Version](https://img.shields.io/badge/python-3.8.5-blue)
+![Django: Version](https://img.shields.io/badge/Django-3.1.3-blue)
+
+# Itrencotech-web-page
+Itrencotech web page using django
+
+## Built With
+<details> <summary> <b> Develop Stack   </b></summary>
+</br>
+
+* ![Python](https://img.shields.io/badge/-Python-000000?style=flat&logo=python)
+* ![Django](https://img.shields.io/badge/-Django-000000?style=flat&logo=django)
+* ![MySQL](https://img.shields.io/badge/-MySQL-000000?style=flat&logo=mysql)  
+* ![Jira](https://img.shields.io/badge/-Jira-000000?style=flat&logo=jira)  
+* ![AWS S3](https://img.shields.io/badge/-S3-000000?style=flat&logo=amazon-aws)  
+* ![AWS EC2](https://img.shields.io/badge/-EC2-000000?style=flat&logo=amazon-aws)  
+
+</details>
+</br>
+
+
+## Basic Layout
+![image](https://user-images.githubusercontent.com/70627982/147803732-41ea3a8c-c93c-4832-a33c-f4fca5f8637d.png)
+
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contributor
+* [bjo6300](https://github.com/bjo6300) <br>
+* [nhk1657](https://github.com/nhk1657) <br>
+* [yeseong31](https://github.com/yeseong31) <br>
+* [eneleh758](https://github.com/eneleh758) <br>
---
+++ itrencotech/views.py
@@ -0,0 +1,113 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+
+def index(request):
+    return render(request, 'body/main.html')
+
+
+def portfolio(request):
+    """ 포트폴리오 페이지 """
+    return render(request, 'navBar/portfolio/portfolio.html')
+
+
+def portfolio3d(request):
+    """ 포트폴리오 - 3D 프린팅 페이지 """
+    return render(request, 'navBar/portfolio/3d.html')
+
+
+def portfolioCnc(request):
+    """ 포트폴리오 - CNC 페이지 """
+    return render(request, 'navBar/portfolio/cnc.html')
+
+
+def portfolioMockup(request):
+    """ 포트폴리오 - 목업 페이지 """
+    return render(request, 'navBar/portfolio/mockup.html')
+
+
+def portfolioMold(request):
+    """ 포트폴리오 - 사출 금형 페이지 """
+    return render(request, 'navBar/portfolio/mold.html')
+
+
+def review(request):
+    """ 후기 페이지 """
+    return render(request, 'navBar/review/review.html')
+
+
+def body_onlyIdea_onlyIdea(request):
+    return render(request, 'body/onlyIdea/onlyIdea_home.html')
+
+
+def body_prototyping_prototyping_home(request):
+    # 시제품 제작 메인
+    return render(request, 'body/prototyping/prototyping_home.html')
+
+
+def body_onlyIdea_onlyIdea_mechanical_design(request): 
+    # 기계설계
+    return render(request, 'body/onlyIdea/onlyIdea_mechanical_design.html')
+
+
+def body_onlyIdea_onlyIdea_product_mechanism_design(request):
+    # 제품 기구설계
+    return render(request, 'body/onlyIdea/onlyIdea_product_mechanism_design.html')
+
+
+def body_onlyIdea_onlyIdea_production_design(request):
+    # 제품 디자인
+    return render(request, 'body/onlyIdea/onlyIdea_production_design.html')
+
+
+def body_prototyping_3Dprinting(request):
+    # 3D 프린팅
+    return render(request, 'body/prototyping/3DPrinting.html')
+
+
+def body_prototyping_mockUp(request):
+    # 목업
+    return render(request, 'body/prototyping/mockUp.html')
+
+
+def body_prototyping_CNC(request):
+    # CNC 가공
+    return render(request, 'body/prototyping/CNC.html')
+
+
+def body_prototyping_sheeting_bending(request):
+    # 판금 절곡
+    return render(request, 'body/prototyping/sheeting_bending.html')
+
+
+def navbar_about_ceo(request):
+    return render(request, 'navBar/about/ceo.html')
+
+
+def navbar_about_history(request):
+    return render(request, 'navBar/about/history.html')
+
+
+def navbar_about_ideology(request):
+    return render(request, 'navBar/about/ideology.html')
+
+
+def navbar_about_route(request):
+    return render(request, 'navBar/about/route.html')
+
+
+def body_production_home(request):
+    return render(request, 'body/production/home.html')
+
+
+def body_production_mold(request):
+    return render(request, 'body/production/mold.html')
+
+
+def body_production_cnc(request):
+    return render(request, 'body/production/cnc.html')
+
+
+def body_production_press(request):
+    return render(request, 'body/production/press.html')
+
