1b835c137d50f6ed89429d38cad07b7f2f291583
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-01-20 14:03:46 +0900

    main 큰 카테고리 이미지 크기 수정 / 마우스오버 글자 크기 수정 / 포트폴리오 이미지 수정

--- itrencotech/models.py
+++ itrencotech/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# 카테고리 모델  ----------------------------------------------
+
+class CategoryModel(models.Model):
+    category_index = models.AutoField(verbose_name='카테고리 인덱스', primary_key=True)
+    category_main = models.CharField(max_length=50, verbose_name='메인 카테고리')
+    category_sub = models.CharField(max_length=50, verbose_name='서브 카테고리')
+
+    def __str__(self):
+        return self.category_index
+
+    class Meta:  # 메타 클래스를 이용하여 테이블명 지정
+        db_table = 'category'
