ebedf201dc965ba29a2b3190c1d5db51fec22c17
yeseong31 <yeseong31@naver.com>
2022-02-07 16:38:45 +0900

    0207 비밀번호 찾기 - 인증번호 발송까지

--- static/js/confirmPasswd.js
+++ static/js/confirmPasswd.js
@@ -3,8 +3,10 @@ var check = function() {
         document.getElementById('input-showed-02').value) {
         document.getElementById('span-confirm-message').style.color = 'green';
         document.getElementById('span-confirm-message').innerHTML = '✔ 비밀번호가 일치합니다.';
+        document.getElementById('btn-confirm-02').disabled = false;
     } else {
         document.getElementById('span-confirm-message').style.color = 'red';
         document.getElementById('span-confirm-message').innerHTML = '✘ 비밀번호가 일치하지 않습니다.';
+        document.getElementById('btn-confirm-02').disabled = true;
     }
 }
