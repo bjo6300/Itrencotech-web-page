d1cd6a11f2d86d05a86d489ee5d883cade425509
yeseong31 <yeseong31@naver.com>
2022-02-12 21:18:40 +0900

    0212 commit

--- itrencotech/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from itrencotech.models import Category, Portfolio
-
-admin.site.register(Portfolio)
--- static/js/mouseover.js
+++
@@ -1,9 +0,0 @@
-// 마우스 오버 버튼
-
-
-$(".hover").mouseleave(
-  function() {
-    $(this).removeClass("hover");
-  }
-);
-
