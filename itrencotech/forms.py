e5d3fb961838df52e0369c3245bd9dd97e05c43f
yeseong31 <yeseong31@naver.com>
2022-02-19 01:03:51 +0900

    0219 commit

---
+++ itrencotech/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+# class OrderFileForm(forms.ModelForm):
+#     class Meta:
+#         model = OrderFileModel
+#         fields = ('userid', 'order_num', 'filename', 'file')
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.fields['file'].required = False
