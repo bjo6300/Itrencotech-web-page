20cfe12eb52b39eb4d07231ca589c3330b29e6cf
yeseong31 <yeseong31@naver.com>
2022-02-19 01:04:42 +0900

    0219 commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'c=%dig$7ojo8cksb_p+q!)^98te@$e$-jzna&l#6*+)36t369c')
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))    # 0218 변경(django_heroku)
