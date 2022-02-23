d1621ea51ef339265935262897a4bc508bfbb72d
yeseong31 <yeseong31@naver.com>
2022-02-23 15:43:11 +0900

    0223 my_settings 추가

---
+++ config/my_settings.py
@@ -0,0 +1,33 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # mysqlclient 라이브러리 설치
+        'NAME': 'itrencotech_database',  # 생성한 database의 이름
+        'USER': 'root',
+        'PASSWORD': '!Roffjrtl6973?',  # mysql 설치 시 입력한 root 비밀번호 입력
+        'HOST': 'localhost',
+        'PORT': '3306',  # Port 번호
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8',
+            'use_unicode': True,
+        }
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = {
+    'secret': 'c=%dig$7ojo8cksb_p+q!)^98te@$e$-jzna&l#6*+)36t369c',
+}
+
+EMAIL = {
+    'EMAIL_BACKEND': 'django.core.mail.backends.smtp.EmailBackend',
+    'EMAIL_USE_TLS': True,  # TLS 보안 설정
+    'EMAIL_PORT': '587',
+    'EMAIL_HOST': 'smtp.gmail.com',
+    'EMAIL_HOST_USER': 'kodealtest@gmail.com',
+    'EMAIL_HOST_PASSWORD': '!zhelf17?',
+    'SERVER_EMAIL': 'kodealtest',
+    'REDIRECT_PAGE': 'http://127.0.0.1:8000/home/'
+}
+
+
