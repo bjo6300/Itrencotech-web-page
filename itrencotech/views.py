17e143c1a00d564ca152ead5bbeb689fbfd4bc51
yeseong31 <yeseong31@naver.com>
2022-02-28 15:27:44 +0900

    0228 navBar → navbar로 변경

--- itrencotech/views.py
+++ itrencotech/views.py
@@ -19,7 +19,7 @@ def index(request):
 
 def portfolio(request):
     """ 포트폴리오 페이지 """
-    return render(request, 'navBar/portfolio/portfolio.html')
+    return render(request, 'navbar/portfolio/portfolio.html')
 
 
 def portfolioDesign(request):
@@ -32,7 +32,7 @@ def portfolioDesign(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/design.html', context)
+    return render(request, 'navbar/portfolio/design.html', context)
 
 
 def portfolioMachineDesign(request):
@@ -45,7 +45,7 @@ def portfolioMachineDesign(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/machine_design.html', context)
+    return render(request, 'navbar/portfolio/machine_design.html', context)
 
 
 def portfolioHWmachine(request):
@@ -58,7 +58,7 @@ def portfolioHWmachine(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/HW_machine.html', context)
+    return render(request, 'navbar/portfolio/HW_machine.html', context)
 
 
 def portfolio3d(request):
@@ -71,7 +71,7 @@ def portfolio3d(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/3d.html', context)
+    return render(request, 'navbar/portfolio/3d.html', context)
 
 
 def portfolioMockup(request):
@@ -84,7 +84,7 @@ def portfolioMockup(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/mockup.html', context)
+    return render(request, 'navbar/portfolio/mockup.html', context)
 
 
 def portfolioCnc(request):
@@ -97,7 +97,7 @@ def portfolioCnc(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/cnc.html', context)
+    return render(request, 'navbar/portfolio/cnc.html', context)
 
 
 def portfolioSdm(request):
@@ -110,7 +110,7 @@ def portfolioSdm(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/sdm.html', context)
+    return render(request, 'navbar/portfolio/sdm.html', context)
 
 
 def portfolioMold(request):
@@ -123,7 +123,7 @@ def portfolioMold(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/mold.html', context)
+    return render(request, 'navbar/portfolio/mold.html', context)
 
 
 def portfolioInjectionMolding(request):
@@ -136,7 +136,7 @@ def portfolioInjectionMolding(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/injection_molding.html', context)
+    return render(request, 'navbar/portfolio/injection_molding.html', context)
 
 
 def portfolioEquipmentDesign(request):
@@ -149,27 +149,27 @@ def portfolioEquipmentDesign(request):
     page_obj = paginator.get_page(page)
 
     context = {'portfolio_list': page_obj}
-    return render(request, 'navBar/portfolio/equipment_design.html', context)
+    return render(request, 'navbar/portfolio/equipment_design.html', context)
 
 
 def reviewBest(request):
     """ 후기 - 베스트후기 페이지 """
-    return render(request, 'navBar/review/review_best.html')
+    return render(request, 'navbar/review/review_best.html')
 
 
 def reviewRecent(request):
     """ 후기 - 최신순 페이지 """
-    return render(request, 'navBar/review/review_recent.html')
+    return render(request, 'navbar/review/review_recent.html')
 
 
 def reviewHighRating(request):
     """ 후기 - 높은평점순 페이지 """
-    return render(request, 'navBar/review/review_high_rating.html')
+    return render(request, 'navbar/review/review_high_rating.html')
 
 
 def reviewLowRating(request):
     """ 후기 - 낮은평점순 페이지 """
-    return render(request, 'navBar/review/review_low_rating.html')
+    return render(request, 'navbar/review/review_low_rating.html')
 
 
 def body_onlyIdea_onlyIdea(request):
@@ -194,23 +194,23 @@ def prototyping_detail(request):
 
 
 def navbar_about_ceo(request):
-    return render(request, 'navBar/about/ceo.html')
+    return render(request, 'navbar/about/ceo.html')
 
 
 def navbar_about_history(request):
-    return render(request, 'navBar/about/history.html')
+    return render(request, 'navbar/about/history.html')
 
 
 def navbar_about_ideology(request):
-    return render(request, 'navBar/about/ideology.html')
+    return render(request, 'navbar/about/ideology.html')
 
 
 def navbar_about_route(request):
-    return render(request, 'navBar/about/route.html')
+    return render(request, 'navbar/about/route.html')
 
 
 def navbar_about_test(request):
-    return render(request, 'navBar/about/test.html')
+    return render(request, 'navbar/about/test.html')
 
 
 def body_production_home(request):
@@ -348,12 +348,12 @@ def mypage_home(request):
 
 
 def mypage_order_history(request):
-    return render(request, 'navBar/mypage/mypage_order_history.html')
+    return render(request, 'navbar/mypage/mypage_order_history.html')
 
 
 def mypage_update_info(request):
-    return render(request, 'navBar/mypage/mypage_update_info.html')
+    return render(request, 'navbar/mypage/mypage_update_info.html')
 
 
 def mypage_info(request):
-    return render(request, 'navBar/mypage/mypage_info.html')
+    return render(request, 'navbar/mypage/mypage_info.html')
